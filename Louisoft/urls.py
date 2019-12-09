d70950e1d8c4b23f57b1e5ea8a8ba0b78c4bb8bb
Louieee <louis.paul9095@gmail.com>
2019-12-10 00:12:29 +0100

    added my cv

--- Louisoft/urls.py
+++ Louisoft/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .view import index
+from .view import index, cv
 from Customer.views import send_message
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
-    path('email/', send_message, name="email")
+    path('email/', send_message, name="email"),
+    path('resumee/', cv, name="resumee")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
