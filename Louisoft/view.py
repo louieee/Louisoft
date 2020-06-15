57d8cb65e0a35cab36cb1a215e609bea41068771
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-06-15 21:34:08 +0100

    major change

--- Louisoft/view.py
+++ Louisoft/view.py
@@ -6,10 +6,10 @@ def index(request):
         if 'message' in request.session:
             data = {'message': request.session['message'], 'status': request.session['status']}
             del request.session['message'], request.session['status']
-            return render(request, '../templates/Louisoft/index.html', data)
+            return render(request, 'Louisoft/index.html', data)
 
-    return render(request, '../templates/Louisoft/index.html')
+        return render(request, 'Louisoft/index.html')
 
 
 def cv(request):
-    return render(request, '../templates/Louisoft/resumee.html')
+    return render(request, 'Louisoft/resumee.html')
